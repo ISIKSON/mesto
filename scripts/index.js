0a1aa412f7195c0b5193b40153d50f201fb1d865
Islam Klindzhov <klindzhov@mail.ru>
2021-11-24 18:07:20 +0400

    mesto-russia

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,20 @@
-const aboutLink= document.querySelector(".profile__edit-button_type_about");
-const popup= document.querySelector(".popup");
-const popupCloseButton= popup.querySelector(".popup__close-button");
-const popupOverlay= popup.querySelector(".popup__overlay");
-const popupForm= popup.querySelector(".popup__container");
-const nameInput= popup.querySelector(".popup__input_type_name");
-const jobInput= popup.querySelector(".popup__input_type_job");
-const saveButton=popup.querySelector(".popup__button");
-const nameProfile= document.querySelector(".profile__title");
-const jobProfile= document.querySelector(".profile__subtitle");
+const aboutLink= document.querySelector('.profile__edit-button_type_about');
+const popup= document.querySelector('.popup');
+const popupCloseButton= popup.querySelector('.popup__close-button');
+const popupForm= popup.querySelector('.popup__container');
+const nameInput= popup.querySelector('.popup__input_type_name');
+const jobInput= popup.querySelector('.popup__input_type_job');
+const nameProfile= document.querySelector('.profile__title');
+const jobProfile= document.querySelector('.profile__subtitle');
 
 function openPopup() {
-    popup.classList.add("popup__opened");
+    popup.classList.add('popup_opened');
     nameInput.value=nameProfile.textContent;
     jobInput.value=jobProfile.textContent;
 }
 
 function closePopup() {
-    popup.classList.remove("popup__opened");
+    popup.classList.remove('popup_opened');
 }
 
 function formSubmitHandler (evt) {
@@ -28,5 +26,4 @@ function formSubmitHandler (evt) {
 
 aboutLink.addEventListener('click', openPopup);
 popupCloseButton.addEventListener('click', closePopup);
-popupOverlay.addEventListener('click', closePopup);
 popupForm.addEventListener('submit', formSubmitHandler); 
